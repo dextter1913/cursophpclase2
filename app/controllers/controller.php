243dacc952babc839dad522d29f6a80425d77ca2
dextter1913 <dextter1913@gmail.com>
2021-10-20 20:18:07 -0500

    Agregando condicionales

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -15,10 +15,20 @@ function piedepagina(){
 
 class controller
 {
+    //Inicio
     public static function Inicio(){
         cabecera();
         Nav();
         require_once 'app\views\modules\inicio.phtml';
+        piedepagina();
+    }
+
+
+    //Condicionales
+    public static function Condicionales(){
+        cabecera();
+        Nav();
+        require_once 'app\views\modules\condicionales.phtml';
         piedepagina();
     }
 }
